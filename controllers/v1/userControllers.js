4ad414443d58304fe6b57100d44a2a2dcc6bbc8e
Bagus Dwi <sugab.dwi88@gmail.com>
2024-03-29 13:27:53 +0700

    API User

--- controllers/v1/userControllers.js
+++ controllers/v1/userControllers.js
@@ -4,7 +4,8 @@ const prisma = new PrismaClient();
 module.exports = {
   store: async (req, res, next) => {
     try {
-      let { name, email, password } = req.body;
+      let { name, email, password, identity_type, identity_number, address } =
+        req.body;
 
       let exist = await prisma.user.findFirst({
         where: { email },
@@ -36,6 +37,46 @@ module.exports = {
         message: "success",
         data: user,
       });
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
+  },
+  index: async (req, res, next) => {
+    try {
+      let user = await prisma.user.findMany();
+      res.status(201).json({
+        status: true,
+        message: "success",
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  show: async (req, res, next) => {
+    try {
+      let id = Number(req.params.id);
+      let user = await prisma.user.findUnique({
+        where: { id }, 
+        include: {
+          profiles: true,
+        },
+      });
+
+      if (!user) {
+        return res.status(400).json({
+          status: false,
+          message: "can\t find user with id " + id,
+          data: null,
+        });
+      }
+      res.status(200).json({
+        status: true,
+        message: "success",
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
   },
 };
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const userControllers = require("../../controllers/v1/userControllers");
 router.post("/users", userControllers.store);
-// router.get("/users", userControllers.);
-// router.get()
+router.get("/users", userControllers.index);
+router.get("/users/:id", userControllers.show);
 
 module.exports = router;
